d26f5443ff470e4f24e2cf861287b67ae0278b06
Tiffany <hainguyen17@augustana.edu>
2021-03-17 20:50:54 -0500

    add grid demonstration

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,25 +1,34 @@
+import { loremIpsum } from 'lorem-ipsum';
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import './Layout.css';
 
-const styles = {
-  column: {
-    border: '1px solid #ccc',
-    height: '5rem'
-  }
-};
 export const LayoutPage = () => (
   <>
-    <Row>
-      <Col style={styles.column} md={12}>
+    <Row className='py-2'>
+      <Col className='column' md={12}>
         12
       </Col>
     </Row>
-    <Row>
-      <Col style={styles.column} md={6} sm={12}>
+    <Row className='py-2'>
+      <Col className='column' md={6} sm={12}>
         6 in desktop view, 12 in mobile view
       </Col>
-      <Col style={styles.column} md={6} sm={12}>
+      <Col className='column' md={6} sm={12}>
         6 in desktop view, 12 in mobile view
+      </Col>
+    </Row>
+    <Row className='py-2'>
+      <Col md={6} sm={12}>
+        <img
+          alt={'cat'}
+          className='image'
+          src={'https://i.pinimg.com/originals/c9/37/33/c937334e6b6781c3f494dda3064eb1e6.jpg'}
+        />
+      </Col>
+      <Col md={6} sm={12}>
+        <h1>Cute little cats!</h1>
+        <p>{loremIpsum()}</p>
       </Col>
     </Row>
   </>
