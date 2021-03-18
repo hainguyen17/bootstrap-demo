a63b4cfc21ea608a8f90ef481dc7e13b8885f77d
Tiffany <hainguyen17@augustana.edu>
2021-03-18 10:05:02 -0500

    revert routing

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,21 +2,6 @@ import React from 'react';
 import {Navbar, Nav, Form, Button, FormControl } from 'react-bootstrap';
 import { useHistory, useLocation } from 'react-router-dom';
 
-const routes = [
-  {
-    name: 'Layout',
-    path: '/layout'
-  },
-  {
-    name: 'Sign Up',
-    path: '/signup'
-  },
-  {
-    name: 'Login',
-    path: '/login'
-  }
-];
-
 export const Header = () => {
   const history = useHistory();
   const {pathname} = useLocation();
@@ -25,16 +10,24 @@ export const Header = () => {
     <Navbar bg="dark" variant="dark" className="mb-2">
       <Navbar.Brand href="/">Navbar</Navbar.Brand>
       <Nav className="mr-auto">
-        {
-          routes.map((route) =>
-            <Nav.Link
-              active={route.path === pathname}
-              onClick={() => history.push(route.path)}
-            >
-              {route.name}
-            </Nav.Link>
-          )
-        }
+          <Nav.Link
+            active={pathname === '/layout'}
+            onClick={() => history.push('/layout')}
+          >
+            Layout
+          </Nav.Link>
+          <Nav.Link
+            active={pathname === '/signup'}
+            onClick={() => history.push('/signup')}
+          >
+            Sign Up
+          </Nav.Link>
+          <Nav.Link
+            active={pathname === '/login'}
+            onClick={() => history.push('/login')}
+          >
+            Login
+          </Nav.Link>
       </Nav>
       <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
